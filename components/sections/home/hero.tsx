a3396d355a0de20f6113dc4bfc13191658937941
Nazareno Oviedo <nazareno@basement.studio>
2020-06-29 18:04:51 -0300

    feat: add gsap implementation

--- components/sections/home/hero.tsx
+++ components/sections/home/hero.tsx
@@ -1,42 +1,63 @@
 /** @jsx jsx */
+import { useRef, useEffect } from "react"
 import { jsx, Container, Heading, Text, Button, Box } from "theme-ui"
+
+import { gsap } from "gsap"
+import { DURATION } from "lib/animations"
+
 import { Divider } from "components/primitives/divider"
 
-const HomeHero = () => (
-  <Box sx={{ bg: "muted" }}>
-    <Container
-      variant="section"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        pt: 5
-      }}
-    >
-      <Box sx={{ maxWidth: "4xl", mb: ["32px", "40px"] }}>
-        <Heading
-          variant="heading.1"
-          sx={{ fontSize: ["40px", "56px", "88px"] }}
-        >
-          The&nbsp;
-          <Text as="span" variant="gradient">
-            World's Open&nbsp;
-          </Text>
-          <br sx={{ display: ["none", "block"] }} />
-          Video Infrascructure
-        </Heading>
-        <Divider isTransparent isVertical size={["12px", "16px", "24px"]} />
-        <Heading variant="section.subtitle">
-          Livepeer supports live streaming, video on demand, and transcoding
-          across video formats and protocols.
-        </Heading>
-      </Box>
-      <Button>Get started</Button>
-      <Box
-        sx={{ bg: "ultraLightGray", height: "500px", width: "100%", my: 4 }}
-      />
-    </Container>
-  </Box>
-)
+const HomeHero = () => {
+  const section = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const tl = gsap.timeline()
+
+    tl.set(section.current, {
+      autoAlpha: 0
+    })
+
+    //@ts-ignore
+    tl.sectionFadeIn(section.current, { duration: DURATION })
+  }, [])
+  return (
+    <Box sx={{ bg: "muted" }}>
+      <Container
+        variant="section"
+        ref={section}
+        className="hide__section"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          pt: 5
+        }}
+      >
+        <Box sx={{ maxWidth: "4xl", mb: ["32px", "40px"] }}>
+          <Heading
+            variant="heading.1"
+            sx={{ fontSize: ["40px", "56px", "88px"] }}
+          >
+            The&nbsp;
+            <Text as="span" variant="gradient">
+              World's Open&nbsp;
+            </Text>
+            <br sx={{ display: ["none", "block"] }} />
+            Video Infrascructure
+          </Heading>
+          <Divider isTransparent isVertical size={["12px", "16px", "24px"]} />
+          <Heading variant="section.subtitle">
+            Livepeer supports live streaming, video on demand, and transcoding
+            across video formats and protocols.
+          </Heading>
+        </Box>
+        <Button>Get started</Button>
+        <Box
+          sx={{ bg: "ultraLightGray", height: "500px", width: "100%", my: 4 }}
+        />
+      </Container>
+    </Box>
+  )
+}
 
 export default HomeHero
