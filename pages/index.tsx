c4c140f6d1fbc72fbc8e3611d242bd1565f964ec
Julián Benegas <julianbenegas99@gmail.com>
2020-06-29 11:40:50 -0300

    improve component structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
 import { Box } from "theme-ui"
 import Head from "components/primitives/head"
 import Nav from "components/sections/nav"
-import HeroHome from "components/sections/heroes/home"
+import HomeHero from "components/sections/home/hero"
 import Footer from "components/sections/footer"
-import ByTheNumbersSection from "components/sections/by-the-numbers"
-import CarsSection from "components/sections/cards"
-import PrimerBanner from "components/sections/primer-banner"
-import CommunitySection from "components/sections/community"
+import ByTheNumbersSection from "components/sections/home/by-the-numbers"
+import CarsSection from "components/sections/home/cards"
+import PrimerBanner from "components/sections/home/primer-banner"
+import CommunitySection from "components/sections/home/community"
 
 const HomePage = () => (
   <>
     <Head />
     <Nav />
     <Box sx={{ position: "relative", overflow: "hidden" }}>
-      <HeroHome />
+      <HomeHero />
       <CarsSection />
       <PrimerBanner />
       <ByTheNumbersSection />
