c4ff5ecfc46abbd19d1a305374b7e4769aa9ff43
Julián Benegas <julianbenegas99@gmail.com>
2020-07-03 16:24:01 -0300

    add page layout

--- pages/code.tsx
+++ pages/code.tsx
@@ -1,22 +1,14 @@
-import { Box } from "theme-ui"
-import Head from "components/primitives/head"
-import Nav from "components/sections/nav"
-import Footer from "components/sections/footer"
 import CodeHero from "components/sections/code/hero"
 import OpenSourceSection from "components/sections/code/open-source"
 import GetInvolvedSection from "components/sections/code/get-involved"
+import PageLayout from "components/layouts/page"
 
 const CodePage = () => (
-  <>
-    <Head />
-    <Nav isDark />
-    <Box sx={{ position: "relative", overflow: "hidden", bg: "text" }}>
-      <CodeHero />
-      <OpenSourceSection />
-      <GetInvolvedSection />
-    </Box>
-    <Footer isDark />
-  </>
+  <PageLayout pushContentSx={{ bg: "text" }} isDark>
+    <CodeHero />
+    <OpenSourceSection />
+    <GetInvolvedSection />
+  </PageLayout>
 )
 
 export default CodePage
--- pages/developers.tsx
+++ pages/developers.tsx
@@ -1,24 +1,16 @@
-import { Box } from "theme-ui"
-import Head from "components/primitives/head"
-import Nav from "components/sections/nav"
-import Footer from "components/sections/footer"
 import DevelopersHero from "components/sections/developers/hero"
 import PublicNetworkBanner from "components/sections/developers/public-network-banner"
 import HostedApiSection from "components/sections/developers/hosted-api"
 import IsOpenSourceSection from "components/sections/developers/is-open-source"
+import PageLayout from "components/layouts/page"
 
 const CodePage = () => (
-  <>
-    <Head />
-    <Nav />
-    <Box sx={{ position: "relative", overflow: "hidden", bg: "text" }}>
-      <DevelopersHero />
-      <PublicNetworkBanner />
-      <HostedApiSection />
-      <IsOpenSourceSection />
-    </Box>
-    <Footer />
-  </>
+  <PageLayout pushContentSx={{ bg: "text" }}>
+    <DevelopersHero />
+    <PublicNetworkBanner />
+    <HostedApiSection />
+    <IsOpenSourceSection />
+  </PageLayout>
 )
 
 export default CodePage
