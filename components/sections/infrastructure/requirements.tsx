ffec5ed76f6aa4ee008741ff685ccc72c9fd8f91
Julián Benegas <julianbenegas99@gmail.com>
2020-07-03 17:27:53 -0300

    init rest of sections

---
+++ components/sections/infrastructure/requirements.tsx
@@ -0,0 +1,14 @@
+import SectionLayout from "components/layouts/section"
+
+const RequirementsSection = () => (
+  <SectionLayout
+    title="Requirements"
+    background="dark"
+    subtitle="To become an infrastructure operator, you must have the following"
+    pushSx={{ py: ["80px", null, null, "160px"] }}
+  >
+    Content
+  </SectionLayout>
+)
+
+export default RequirementsSection
