9281aa806f51f735a2a67dc50cf7d9b409bd0d24
Julián Benegas <julianbenegas99@gmail.com>
2020-07-06 09:53:20 -0300

    finish /infrastructure page

---
+++ components/sections/infrastructure/how-to-get-started/index.tsx
@@ -0,0 +1,87 @@
+/** @jsx jsx */
+import { jsx, Box, Heading, Link as A, SxStyleProp, Grid } from "theme-ui"
+import HowToGetStartedStep, { HowToGetStartedStepProps } from "./step"
+
+const linkSx: SxStyleProp = {
+  color: "secondary",
+  fontWeight: 600
+}
+
+const steps: HowToGetStartedStepProps[] = [
+  {
+    children: (
+      <>
+        Read <A sx={linkSx}>this tutorial</A>
+      </>
+    )
+  },
+  {
+    children: (
+      <>
+        View the <A sx={linkSx}>node operation docs</A> and the{" "}
+        <A sx={linkSx}>GPU scaling docs</A>.
+      </>
+    )
+  },
+  {
+    children: (
+      <>
+        Reach out to the Livepeer team and community in the{" "}
+        <A sx={linkSx}>Discord chat room</A>, #transcoding channel. Initial
+        testing and setup can require some DevOps, so we are happy to help.
+      </>
+    )
+  },
+  {
+    children: (
+      <>
+        Review the <A sx={linkSx}>node operation FAQ</A>.
+      </>
+    )
+  }
+]
+
+const HowToGetStartedSection = () => (
+  <Box>
+    <Grid
+      variant="layout.section"
+      gap={5}
+      columns={[1, null, 2]}
+      sx={{ pt: ["80px", null, null, "160px"] }}
+    >
+      <Box>
+        <Heading
+          variant="section.title"
+          sx={{ textAlign: ["center", null, "left"] }}
+        >
+          How to get started
+        </Heading>
+        <Heading
+          variant="section.subtitle"
+          sx={{ textAlign: ["center", null, "left"] }}
+        >
+          Become an infrastructure operator in four steps.
+        </Heading>
+      </Box>
+      <Box
+        sx={{
+          ".list-item:not(:last-of-type)": {
+            mb: 2
+          }
+        }}
+      >
+        {steps.map((s, i, { length }) => (
+          <HowToGetStartedStep
+            key={`how-to-get-started-step-${i}`}
+            className="list-item"
+            index={i}
+            isLast={length === i + 1}
+            {...s}
+          />
+        ))}
+      </Box>
+    </Grid>
+  </Box>
+)
+
+export default HowToGetStartedSection
