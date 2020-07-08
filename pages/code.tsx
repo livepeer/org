539017f1b5dd95036e7b9a3e082203477ef21914
Julián Benegas <julianbenegas99@gmail.com>
2020-07-08 10:17:05 -0300

    update code hero

--- pages/code.tsx
+++ pages/code.tsx
@@ -1,11 +1,62 @@
-import CodeHero from "components/sections/code/hero"
+/** @jsx jsx */
+import { jsx, Box } from "theme-ui"
 import OpenSourceSection from "components/sections/code/open-source"
 import GetInvolvedSection from "components/sections/code/get-involved"
 import PageLayout from "components/layouts/page"
+import IconHero from "components/layouts/icon-hero"
+import { FiHeart } from "react-icons/fi"
+
+const CoverImage = () => (
+  <figure
+    sx={{
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%,-50%)",
+      userSelect: "none"
+    }}
+  >
+    <img
+      sx={{ minWidth: "1440px", maxWidth: "1440px" }}
+      src="/images/contributors.png"
+    />
+    <Box
+      sx={{
+        position: "absolute",
+        top: 0,
+        minWidth: "1440px",
+        maxWidth: "1440px",
+        height: "270px",
+        left: "50%",
+        transform: "translateX(-50%)",
+        background:
+          "linear-gradient(90deg, #131418 18.82%, rgba(19, 20, 24, 0) 100%)"
+      }}
+    />
+    <Box
+      sx={{
+        position: "absolute",
+        top: 0,
+        minWidth: "1440px",
+        maxWidth: "1440px",
+        height: "270px",
+        left: "50%",
+        transform: "translateX(-50%)",
+        background: "rgba(19, 20, 24, 0.7)"
+      }}
+    />
+  </figure>
+)
 
 const CodePage = () => (
   <PageLayout pushContentSx={{ bg: "text" }} isDark>
-    <CodeHero />
+    <IconHero
+      icon={<FiHeart />}
+      title="Livepeer open source"
+      subtitle="We believe in open components for an open video infrastructure and an open web."
+      illustration={<CoverImage />}
+      background="dark"
+    />
     <OpenSourceSection />
     <GetInvolvedSection />
   </PageLayout>
