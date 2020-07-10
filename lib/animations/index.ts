90c0f2877f36441efdbebef3ba47f5ff7cc98ba9
Nazareno Oviedo <nazareno@basement.studio>
2020-07-10 16:36:07 -0300

    fix: fix gsap imports

--- lib/animations/index.ts
+++ lib/animations/index.ts
@@ -3,77 +3,80 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { SplitText } from "gsap/SplitText"
 
-//@ts-ignore
-gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger, SplitText)
-
 const GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2
 const RECIPROCAL_GR = 1 / GOLDEN_RATIO
 export const DURATION = RECIPROCAL_GR * 0.9
 
-gsap.registerEffect({
-  name: "textEntrance",
-  effect: (targets: gsap.TweenTarget, config: { duration: number }) => {
-    return gsap.to(targets, {
-      autoAlpha: 1,
-      duration: config.duration,
-      ease: "sine.out",
-      y: 0,
-      stagger: {
-        each: 0.1,
-        from: "start"
-      }
-    })
-  },
-  defaults: { duration: DURATION },
-  extendTimeline: true
-})
+const initGsap = () => {
+  gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger, SplitText)
 
-gsap.registerEffect({
-  name: "textHide",
-  effect: (targets: gsap.TweenTarget) => {
-    return gsap.set(targets, { y: 100, autoAlpha: 0 })
-  },
-  extendTimeline: false
-})
-
-gsap.registerEffect({
-  name: "sectionEntrance",
-  effect: (targets: gsap.TweenTarget, config: { duration: number }) => {
-    return gsap.to(targets, {
-      autoAlpha: 1,
-      duration: config.duration
-    })
-  },
-  defaults: { duration: DURATION },
-  extendTimeline: true
-})
-
-gsap.registerEffect({
-  name: "sectionHide",
-  effect: (targets: gsap.TweenTarget) => {
-    return gsap.set(targets, { autoAlpha: 0 })
-  },
-  extendTimeline: false
-})
-
-gsap.registerEffect({
-  name: "elementsEntrance",
-  effect: (targets: gsap.TweenTarget, config: { duration: number }) => {
-    return gsap.fromTo(
-      [targets],
-      { autoAlpha: 0, y: 60 },
-      {
+  gsap.registerEffect({
+    name: "textEntrance",
+    effect: (targets: gsap.TweenTarget, config: { duration: number }) => {
+      return gsap.to(targets, {
         autoAlpha: 1,
         duration: config.duration,
         ease: "sine.out",
         y: 0,
         stagger: {
-          each: config.duration * 0.5,
+          each: 0.1,
           from: "start"
         }
-      }
-    )
-  },
-  defaults: { duration: DURATION },
-  extendTimeline: true
-})
+      })
+    },
+    defaults: { duration: DURATION },
+    extendTimeline: true
+  })
+
+  gsap.registerEffect({
+    name: "textHide",
+    effect: (targets: gsap.TweenTarget) => {
+      return gsap.set(targets, { y: 100, autoAlpha: 0 })
+    },
+    extendTimeline: false
+  })
+
+  gsap.registerEffect({
+    name: "sectionEntrance",
+    effect: (targets: gsap.TweenTarget, config: { duration: number }) => {
+      return gsap.to(targets, {
+        autoAlpha: 1,
+        duration: config.duration
+      })
+    },
+    defaults: { duration: DURATION },
+    extendTimeline: true
+  })
+
+  gsap.registerEffect({
+    name: "sectionHide",
+    effect: (targets: gsap.TweenTarget) => {
+      return gsap.set(targets, { autoAlpha: 0 })
+    },
+    extendTimeline: false
+  })
+
+  gsap.registerEffect({
+    name: "elementsEntrance",
+    effect: (targets: gsap.TweenTarget, config: { duration: number }) => {
+      return gsap.fromTo(
+        [targets],
+        { autoAlpha: 0, y: 60 },
+        {
+          autoAlpha: 1,
+          duration: config.duration,
+          ease: "sine.out",
+          y: 0,
+          stagger: {
+            each: config.duration * 0.5,
+            from: "start"
+          }
+        }
+      )
+    },
+    defaults: { duration: DURATION },
+    extendTimeline: true
+  })
+}
+
+export default initGsap
