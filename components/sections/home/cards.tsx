b81da1b3b6adc6b6acc88e6c0fbcad52329a6875
Nazareno Oviedo <nazareno@basement.studio>
2020-07-10 14:31:47 -0300

    feat: add all sections

--- components/sections/home/cards.tsx
+++ components/sections/home/cards.tsx
@@ -7,8 +7,7 @@ import { Grid } from "theme-ui"
 import IconCard, { IconCardProps } from "components/primitives/cards/icon"
 import { FiUserCheck, FiCode, FiBriefcase } from "react-icons/fi"
 
-gsap.registerPlugin(SplitText)
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const cards: IconCardProps[] = [
   {
--- components/sections/home/community.tsx
+++ components/sections/home/community.tsx
@@ -1,3 +1,4 @@
+import React, { useRef, useEffect } from "react"
 import SectionLayout from "components/layouts/section"
 import { Grid, Flex } from "theme-ui"
 import IconLink, { IconLinkProps } from "components/primitives/links/icon"
@@ -10,6 +11,11 @@ import {
   FaMedium
 } from "react-icons/fa"
 import Divider from "components/primitives/divider"
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { SplitText } from "gsap/SplitText"
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const links: IconLinkProps[] = [
   {
@@ -74,56 +80,100 @@ const links: IconLinkProps[] = [
   }
 ]
 
-const CommunitySection = ({ title = "Community" }) => (
-  <SectionLayout
-    title={title}
-    titleLabel="Subtitle"
-    subtitle="Livepeer is an open project that believes in open source code and creative contribution from people with diverse interests and skill sets. Join us."
-    pushSx={{ pt: "160px" }}
-  >
-    <Grid
-      columns={"repeat(3, 372px)"}
-      gap={[3, null, null, null, 0]}
-      sx={{
-        mx: "auto",
-        justifyContent: "center",
-        gridTemplateColumns: [
-          "372px",
-          null,
-          null,
-          null,
-          ({ space }) => `372px calc(372px + 2 * ${space[3]}px) 372px`
-        ]
-      }}
+const CommunitySection = ({ title = "Community" }) => {
+  const sectionRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!sectionRef.current) return
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: sectionRef.current
+      },
+      onComplete: () => {
+        split.revert()
+      }
+    })
+    tl.set(sectionRef.current, {
+      autoAlpha: 0
+    })
+
+    const items = sectionRef.current.querySelectorAll(".h-animate")
+    const elements = sectionRef.current.querySelectorAll(".c-animate")
+
+    //@ts-ignore
+    const split = new SplitText(items, { type: "lines" })
+
+    // Set overflow text
+    tl.set([items], { overflow: "hidden" })
+    tl.add(gsap.effects.sectionHide(sectionRef.current))
+
+    tl.add(gsap.effects.textHide([split.lines]))
+    //@ts-ignore
+    tl.sectionEntrance(sectionRef.current)
+    //@ts-ignore
+    tl.textEntrance([split.lines])
+    //@ts-ignore
+    tl.elementsEntrance([elements])
+  }, [])
+  return (
+    <SectionLayout
+      className="hide__section"
+      ref={sectionRef}
+      title={title}
+      titleLabel="Subtitle"
+      subtitle="Livepeer is an open project that believes in open source code and creative contribution from people with diverse interests and skill sets. Join us."
+      pushSx={{ pt: "160px" }}
     >
-      {links.map((link, i) => {
-        if (i === 1 || i === 4) {
+      <Grid
+        columns={"repeat(3, 372px)"}
+        gap={[3, null, null, null, 0]}
+        sx={{
+          mx: "auto",
+          justifyContent: "center",
+          gridTemplateColumns: [
+            "372px",
+            null,
+            null,
+            null,
+            ({ space }) => `372px calc(372px + 2 * ${space[3]}px) 372px`
+          ]
+        }}
+      >
+        {links.map((link, i) => {
+          if (i === 1 || i === 4) {
+            return (
+              <Flex key={`icon-link-${link.href}`}>
+                <Divider
+                  isVertical
+                  size="72px"
+                  pushSx={{
+                    mx: 3,
+                    display: ["none", null, null, null, "block"]
+                  }}
+                />
+                <IconLink pushSx={{ width: "372px" }} {...link} />
+                <Divider
+                  isVertical
+                  size="72px"
+                  pushSx={{
+                    mx: 3,
+                    display: ["none", null, null, null, "block"]
+                  }}
+                />
+              </Flex>
+            )
+          }
           return (
-            <Flex key={`icon-link-${link.href}`}>
-              <Divider
-                isVertical
-                size="72px"
-                pushSx={{ mx: 3, display: ["none", null, null, null, "block"] }}
-              />
-              <IconLink pushSx={{ width: "372px" }} {...link} />
-              <Divider
-                isVertical
-                size="72px"
-                pushSx={{ mx: 3, display: ["none", null, null, null, "block"] }}
-              />
-            </Flex>
+            <IconLink
+              key={`icon-link-${link.href}`}
+              pushSx={{ width: "372px" }}
+              {...link}
+            />
           )
-        }
-        return (
-          <IconLink
-            key={`icon-link-${link.href}`}
-            pushSx={{ width: "372px" }}
-            {...link}
-          />
-        )
-      })}
-    </Grid>
-  </SectionLayout>
-)
+        })}
+      </Grid>
+    </SectionLayout>
+  )
+}
 
 export default CommunitySection
