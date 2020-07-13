86b7c7163bd2380183feaafdb414f960ec94d82d
Julián Benegas <julianbenegas99@gmail.com>
2020-07-13 09:31:49 -0300

    icon card clickable

--- components/sections/home/cards.tsx
+++ components/sections/home/cards.tsx
@@ -9,21 +9,30 @@ const cards: IconCardProps[] = [
     title: "Participants",
     description:
       "The core of this open infrastructure is an open, blockchain based network, with 1000's of participants running video infrastructure to enable video streaming at scale.",
-    linkProps: { link: { label: "/participants", href: "/participants" } }
+    linkProps: { link: { label: "/participants", href: "/participants" } },
+    isClickable: true
   },
   {
     icon: <FiCode />,
     title: "Developers",
     description:
       "This open network is built on the open source Livepeer Media Server, which you can build upon in your own video applications whether you want to use the scalable Livepeer network.",
-    linkProps: { link: { label: "/developers", href: "/developers" } }
+    linkProps: { link: { label: "/developers", href: "/developers" } },
+    isClickable: true
   },
   {
     icon: <FiBriefcase />,
     title: "Enterprise",
     description:
       "Video platforms and applications can use this network to transcode live and on demand video at less than 10% of the cost of other solutions.",
-    linkProps: { link: { label: "/enterprise", href: "/enterprise" } }
+    linkProps: {
+      link: {
+        label: "livepeer.com",
+        href: "https://livepeer.com",
+        isExternal: true
+      }
+    },
+    isClickable: true
   }
 ]
 
