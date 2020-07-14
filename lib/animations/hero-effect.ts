8a6b0d00a2cf17c1c59374cc0f53fbc3377c2a6e
Nazareno <nazarenooviedo@gmail.com>
2020-07-14 14:43:46 -0300

    feat: remove text entrance

--- lib/animations/hero-effect.ts
+++ lib/animations/hero-effect.ts
@@ -13,22 +13,10 @@ const heroEffect = (elem: HTMLElement) => {
     autoAlpha: 0
   })
 
-  const items = elem.querySelectorAll(".h-animate")
   const elements = elem.querySelectorAll(".c-animate")
-  const icon = elem.querySelectorAll(".i-animate")
-
-  const split = new SplitText(items, { type: "lines" })
-
-  // Set overflow text
-  tl.set([items], { overflow: "hidden" })
   tl.add(gsap.effects.sectionHide(elem))
-  tl.add(gsap.effects.textHide([split.lines]))
   //@ts-ignore
   tl.sectionEntrance(elem)
-  //@ts-ignore
-  tl.elementsEntrance([icon])
-  //@ts-ignore
-  tl.textEntrance([split.lines])
   //@ts-ignore
   tl.elementsEntrance([elements])
 }
