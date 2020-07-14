f847f92de0964c07fc2604902986e6b67ab9982a
clementroche <rchclement@gmail.com>
2020-07-14 23:31:57 +0200

    Add WebGL animation

---
+++ effects/mosaic/hooks/use-raf.js
@@ -0,0 +1,25 @@
+import gsap from "gsap"
+import RAF from "../js/raf"
+
+let raf
+
+const useRAF = () => {
+  if (!raf) {
+    raf = new RAF()
+
+    gsap.ticker.lagSmoothing(0)
+    gsap.globalTimeline.pause()
+
+    raf.add(
+      "gsap",
+      (clock) => {
+        const time = gsap.globalTimeline.time()
+        gsap.globalTimeline.time(time + clock.deltaTime)
+      },
+      -10
+    )
+  }
+  return raf
+}
+
+export default useRAF
