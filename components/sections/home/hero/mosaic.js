eaf1dd3eb6185fe7883f893de9c2557fa921b2d2
arzafran <franco@basement.studio>
2020-07-15 20:42:19 -0300

    replace video src

--- components/sections/home/hero/mosaic.js
+++ components/sections/home/hero/mosaic.js
@@ -14,8 +14,7 @@ export default function Mosaic() {
     ref.current.appendChild(canvas)
 
     const mosaicEffect = new MosaicEffect([
-      "https://vod-progressive.akamaized.net/exp=1594855173~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F503%2F16%2F402519499%2F1720965098.mp4~hmac=324dc5a9783a7c0d83d8b3591887ce1d195724e53e64b96fd39f94e89a5e4984/vimeo-prod-skyfire-std-us/01/503/16/402519499/1720965098.mp4?filename=mosaic-video-03.mp4",
-      "https://vod-progressive.akamaized.net/exp=1594855192~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F503%2F16%2F402519598%2F1720285069.mp4~hmac=f62b5da13c0b453e20393c1f104d523d1b360a54be1270f6cc4686b1e61fb049/vimeo-prod-skyfire-std-us/01/503/16/402519598/1720285069.mp4?filename=mosaic-video-07.mp4"
+      "/hero-videos/mixkit-one-on-one-basketball-game-751.mp4"
     ])
     scene.add(mosaicEffect)
 
