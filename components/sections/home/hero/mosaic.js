81615bb95ca135bf33fa58414ed30a5aca8ffe30
arzafran <franco@basement.studio>
2020-07-15 16:20:45 -0300

    test new videos

--- components/sections/home/hero/mosaic.js
+++ components/sections/home/hero/mosaic.js
@@ -14,8 +14,8 @@ export default function Mosaic() {
     ref.current.appendChild(canvas)
 
     const mosaicEffect = new MosaicEffect([
-      "https://vod-progressive.akamaized.net/exp=1594775909~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F503%2F16%2F402519529%2F1720284691.mp4~hmac=68b1b45dc57311ede3c4f413d83c275cdb018e820822dd6f9cc4dc7138350af9/vimeo-prod-skyfire-std-us/01/503/16/402519529/1720284691.mp4?filename=mosaic-video-05.mp4",
-      "https://vod-progressive.akamaized.net/exp=1594775949~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F503%2F16%2F402519561%2F1720284872.mp4~hmac=d517eb06ba63445845282cadec2036639b49c5ea6e0cd4916c853ee00277e59e/vimeo-prod-skyfire-std-us/01/503/16/402519561/1720284872.mp4?filename=mosaic-video-06.mp4"
+      "https://vod-progressive.akamaized.net/exp=1594855173~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F503%2F16%2F402519499%2F1720965098.mp4~hmac=324dc5a9783a7c0d83d8b3591887ce1d195724e53e64b96fd39f94e89a5e4984/vimeo-prod-skyfire-std-us/01/503/16/402519499/1720965098.mp4?filename=mosaic-video-03.mp4",
+      "https://vod-progressive.akamaized.net/exp=1594855192~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F503%2F16%2F402519598%2F1720285069.mp4~hmac=f62b5da13c0b453e20393c1f104d523d1b360a54be1270f6cc4686b1e61fb049/vimeo-prod-skyfire-std-us/01/503/16/402519598/1720285069.mp4?filename=mosaic-video-07.mp4"
     ])
     scene.add(mosaicEffect)
 
