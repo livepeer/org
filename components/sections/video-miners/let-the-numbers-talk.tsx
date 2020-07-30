16cb3901ddadb0b4eb9a75e4772536d74b0cd6d7
Adam Soffer <adam@livepeer.org>
2020-07-30 00:23:04 -0400

    add video miner stats (#52)

--- components/sections/video-miners/let-the-numbers-talk.tsx
+++ components/sections/video-miners/let-the-numbers-talk.tsx
@@ -1,45 +1,48 @@
 import { StatProps } from "components/primitives/stat"
 import SectionLayout from "components/layouts/section"
 import StatsGrid from "components/layouts/stats-grid"
+import { nFormatter } from "lib/document-helpers"
 
-const stats: StatProps[] = [
-  {
-    title: "45",
-    label: (
-      <>
-        Total active <br /> nodes
-      </>
-    )
-  },
-  {
-    title: "11.4",
-    label: (
-      <>
-        Total <br /> fees earned
-      </>
-    ),
-    color: "gradient"
-  },
+const LetTheNumbersTalkSection = ({ totalGeneratedFees }) => {
+  const stats: StatProps[] = [
+    {
+      title: "45",
+      label: (
+        <>
+          Total active <br /> nodes
+        </>
+      )
+    },
+    {
+      title: `${nFormatter(totalGeneratedFees, 1)} ETH`,
+      label: (
+        <>
+          Total <br /> fees earned
+        </>
+      ),
+      color: "gradient"
+    },
 
-  {
-    title: "Free",
-    label: (
-      <>
-        Cost to activate <br />
-        your node
-      </>
-    )
-  }
-]
+    {
+      title: "Free",
+      label: (
+        <>
+          Cost to activate <br />
+          your node
+        </>
+      )
+    }
+  ]
 
-const LetTheNumbersTalkSection = () => (
-  <SectionLayout
-    title="The Numbers"
-    subtitle="The capacity on Livepeer's network represents access to 70,000+ GPUs, or enough to encode all the realtime video streaming through Twitch, Facebook, and Youtube combined."
-    pushSx={{ py: ["80px", null, null, "160px"] }}
-  >
-    <StatsGrid stats={stats} />
-  </SectionLayout>
-)
+  return (
+    <SectionLayout
+      title="The Numbers"
+      subtitle="The capacity on Livepeer's network represents access to 70,000+ GPUs, or enough to encode all the realtime video streaming through Twitch, Facebook, and Youtube combined."
+      pushSx={{ py: ["80px", null, null, "160px"] }}
+    >
+      <StatsGrid stats={stats} />
+    </SectionLayout>
+  )
+}
 
 export default LetTheNumbersTalkSection
