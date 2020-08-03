e5cb0ae93385ab71bb80851eb5a787634407fc60
Adam Soffer <adam@livepeer.org>
2020-08-02 23:30:26 -0400

    add GA (#68)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,28 @@ import theme from "lib/theme"
 import "css/global.css"
 import "keen-slider/keen-slider.min.css"
 import initGsap from "lib/animations"
+import * as gtag from "../lib/gtag"
+import { useEffect } from "react"
+import Router from "next/router"
+
 initGsap()
 
-const App = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <Component {...pageProps} />
-  </ThemeProvider>
-)
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    Router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
 
 export default App
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,28 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
+import { GA_TRACKING_ID } from "../lib/gtag"
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
             rel="stylesheet"
