5068d32de52b9831ef2b4b7ccd67d574ce497af9
Julián Benegas <julianbenegas99@gmail.com>
2020-08-03 15:27:06 -0300

    add font-display: auto (#71)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,10 +23,6 @@ export default class MyDocument extends Document {
           `
             }}
           />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
