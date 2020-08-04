45bd54b0b52285c17fa0b1ed72e7c24598e7093c
arzafran <franco@basement.studio>
2020-08-04 00:10:30 -0300

    add meta tags

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -3,9 +3,22 @@ import IconHero from "components/layouts/icon-hero"
 import { FiHelpCircle } from "react-icons/fi"
 import FaqsSection from "components/sections/faqs"
 import FaqHeroSvg from "components/svgs/faq-hero"
+import { HeadProps } from "components/primitives/head"
+
+const headProps: HeadProps = {
+  meta: {
+    title: "Livepeer - participants",
+    description:
+      "Livepeer.org is a primary online resource for participants and users of the Livepeer network.",
+    url: "https://livepeer.org/participants",
+    siteName: "Livepeer.org",
+    image: "https://livepeer.org/OG.png",
+    twitterUsername: "@LivepeerOrg"
+  }
+}
 
 const ParticipantsPage = () => (
-  <PageLayout navProps={{ background: "dark" }}>
+  <PageLayout headProps={headProps} navProps={{ background: "dark" }}>
     <IconHero
       icon={<FiHelpCircle />}
       title="FAQ"
--- pages/video-miners.tsx
+++ pages/video-miners.tsx
@@ -5,9 +5,22 @@ import HowToGetStartedSection from "components/sections/video-miners/how-to-get-
 import RequirementsSection from "components/sections/video-miners/requirements"
 import VideoMinerHero from "components/sections/video-miners/hero"
 import { getTotalGeneratedFees } from "lib/document-helpers"
+import { HeadProps } from "components/primitives/head"
+
+const headProps: HeadProps = {
+  meta: {
+    title: "Livepeer - Video miners",
+    description:
+      "Livepeer.org is a primary online resource for participants and users of the Livepeer network.",
+    url: "https://livepeer.org/video-miners",
+    siteName: "Livepeer.org",
+    image: "https://livepeer.org/OG.png",
+    twitterUsername: "@LivepeerOrg"
+  }
+}
 
 const VideoMinerPage = ({ totalGeneratedFees }) => (
-  <PageLayout>
+  <PageLayout headProps={headProps}>
     <VideoMinerHero />
     <HowVideoMiningWorksSection />
     <LetTheNumbersTalkSection totalGeneratedFees={totalGeneratedFees} />
