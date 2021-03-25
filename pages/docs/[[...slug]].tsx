dca56985d0075109a4fde2adcbd7b7ca765d5692
Julian Benegas <julianbenegas99@gmail.com>
2021-03-25 18:06:15 -0400

    use className instead

--- pages/docs/[[...slug]].tsx
+++ pages/docs/[[...slug]].tsx
@@ -92,7 +92,7 @@ const Docs = ({
         },
       }}>
       <div
-        id="docs-container"
+        className="docs-container"
         sx={{
           maxWidth: "100vw",
           transition: "all 0.2s",
