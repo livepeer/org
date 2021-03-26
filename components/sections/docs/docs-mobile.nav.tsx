4741c2fba33c99c50b84aefa1defd0a67cdfb26b
Julian Benegas <julianbenegas99@gmail.com>
2021-03-26 09:18:41 -0400

    fix double search widget

--- components/sections/docs/docs-mobile.nav.tsx
+++ components/sections/docs/docs-mobile.nav.tsx
@@ -9,7 +9,6 @@ import {
 import MobileLogo from "./mobile-logo";
 import DarkModeSwitch from "components/primitives/darkModeSwitch";
 import DocsMenu, { Menu } from "./docs-menu";
-import { useDocSearch } from "./docs-nav/algolia-doc-search";
 
 type Props = {
   setColorMode: React.Dispatch<React.SetStateAction<string>>;
@@ -61,10 +60,15 @@ const OpenMenu = ({
   );
 };
 
-const DocsMobileNav = ({ setColorMode, colorMode, selected, menu }) => {
+const DocsMobileNav = ({
+  setColorMode,
+  colorMode,
+  selected,
+  menu,
+  searchButtonRef,
+  onOpen,
+}) => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
-
-  const { Modal, onOpen, searchButtonRef } = useDocSearch();
 
   useEffect(() => {
     if (menuOpen) {
@@ -76,89 +80,86 @@ const DocsMobileNav = ({ setColorMode, colorMode, selected, menu }) => {
   }, [menuOpen]);
 
   return (
-    <>
-      <div sx={{ width: "100vw", position: "sticky", top: "0" }}>
+    <div sx={{ width: "100vw", position: "sticky", top: "0" }}>
+      <div
+        sx={{
+          width: "100%",
+          height: "58px",
+          boxShadow:
+            "0px 1px 0px rgba(0, 0, 0, 0.05), 0px 0px 8px rgba(0, 0, 0, 0.03), 0px 30px 30px rgba(0, 0, 0, 0.02)",
+          overflow: "hidden",
+          position: "relative",
+          backgroundColor: "docs.lightBlack",
+          transform: menuOpen ? "translateY(-58px)" : "",
+          transition: "all 0.3s",
+        }}>
         <div
           sx={{
-            width: "100%",
-            height: "58px",
-            boxShadow:
-              "0px 1px 0px rgba(0, 0, 0, 0.05), 0px 0px 8px rgba(0, 0, 0, 0.03), 0px 30px 30px rgba(0, 0, 0, 0.02)",
-            overflow: "hidden",
-            position: "relative",
+            display: "flex",
+            padding: "9px 32px",
+            position: "absolute",
+            left: "0",
             backgroundColor: "docs.lightBlack",
-            transform: menuOpen ? "translateY(-58px)" : "",
-            transition: "all 0.3s",
+            zIndex: "100",
+            transform: "0",
+            justifyContent: "space-between",
+            width: "100vw",
+            height: "100%",
+            alignItems: "center",
+            transition: "all 0.4s",
           }}>
           <div
             sx={{
               display: "flex",
-              padding: "9px 32px",
-              position: "absolute",
-              left: "0",
-              backgroundColor: "docs.lightBlack",
-              zIndex: "100",
-              transform: "0",
-              justifyContent: "space-between",
-              width: "100vw",
-              height: "100%",
               alignItems: "center",
-              transition: "all 0.4s",
+              transition: "all 0.3s",
             }}>
-            <div
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                transition: "all 0.3s",
-              }}>
-              <MobileLogo />
-              <p sx={{ color: "docs.lightGray", fontSize: "16px", ml: "8px" }}>
-                /{" "}
-                <span
-                  sx={{
-                    fontWeight: "bold",
-                    fontSize: "16px",
-                    color: "docs.text",
-                    ml: "6px",
-                  }}>
-                  Docs
-                </span>
-              </p>
-            </div>
-            <div sx={{ display: "flex", alignItems: "center" }}>
-              <button
-                onClick={onOpen}
-                ref={searchButtonRef}
-                sx={{ mr: "6px", p: 2 }}>
-                <SearchIcon color={colorMode === "dark" ? "#fff" : "#131418"} />
-              </button>
-              <button onClick={() => setMenuOpen(true)} sx={{ p: 1 }}>
-                <MobileMenuIcon
-                  color={colorMode === "dark" ? "#fff" : "#131418"}
-                />
-              </button>
-            </div>
+            <MobileLogo />
+            <p sx={{ color: "docs.lightGray", fontSize: "16px", ml: "8px" }}>
+              /{" "}
+              <span
+                sx={{
+                  fontWeight: "bold",
+                  fontSize: "16px",
+                  color: "docs.text",
+                  ml: "6px",
+                }}>
+                Docs
+              </span>
+            </p>
+          </div>
+          <div sx={{ display: "flex", alignItems: "center" }}>
+            <button
+              onClick={onOpen}
+              ref={searchButtonRef}
+              sx={{ mr: "6px", p: 2 }}>
+              <SearchIcon color={colorMode === "dark" ? "#fff" : "#131418"} />
+            </button>
+            <button onClick={() => setMenuOpen(true)} sx={{ p: 1 }}>
+              <MobileMenuIcon
+                color={colorMode === "dark" ? "#fff" : "#131418"}
+              />
+            </button>
           </div>
         </div>
-        <div
-          sx={{
-            transition: "all, 0.3s",
-            opacity: menuOpen ? "1" : "0",
-            transform: menuOpen ? "translateY(-58px)" : "",
-            height: menuOpen ? "100vh" : "0px",
-          }}>
-          <OpenMenu
-            menu={menu}
-            menuOpen={menuOpen}
-            colorMode={colorMode}
-            setColorMode={setColorMode}
-            handleClose={() => setMenuOpen(false)}
-            selected={selected}
-          />
-        </div>
       </div>
-      <Modal />
-    </>
+      <div
+        sx={{
+          transition: "all, 0.3s",
+          opacity: menuOpen ? "1" : "0",
+          transform: menuOpen ? "translateY(-58px)" : "",
+          height: menuOpen ? "100vh" : "0px",
+        }}>
+        <OpenMenu
+          menu={menu}
+          menuOpen={menuOpen}
+          colorMode={colorMode}
+          setColorMode={setColorMode}
+          handleClose={() => setMenuOpen(false)}
+          selected={selected}
+        />
+      </div>
+    </div>
   );
 };
 
--- components/sections/docs/docs-nav/search.tsx
+++ components/sections/docs/docs-nav/search.tsx
@@ -1,76 +1,70 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import { SearchIcon } from "components/svgs/darkMode";
-import { useDocSearch } from "./algolia-doc-search";
 
-const DocsSearch = () => {
-  const { Modal, onOpen, searchButtonRef } = useDocSearch();
-
+const DocsSearch = ({ searchButtonRef, onOpen }) => {
   return (
-    <>
-      <button
+    <button
+      sx={{
+        width: ["250px", "300px", "400px", "700px"],
+        height: "40px",
+        borderRadius: "40px",
+        overflow: "hidden",
+        position: "relative",
+        display: "flex",
+        alignItems: "center",
+      }}
+      ref={searchButtonRef}
+      onClick={onOpen}>
+      <i sx={{ position: "absolute", left: "16px" }}>
+        <SearchIcon color="#A5A5A5" />
+      </i>
+      <span
         sx={{
-          width: ["250px", "300px", "400px", "700px"],
-          height: "40px",
-          borderRadius: "40px",
-          overflow: "hidden",
-          position: "relative",
+          width: "100%",
+          height: "100%",
+          outline: "none",
+          backgroundColor: "docs.muted",
+          transition: "all 0.2s",
+          pl: "42px",
+          pr: "12px",
+          color: "docs.lightGray",
+          fontSize: "16px",
+          fontWeight: "300",
           display: "flex",
           alignItems: "center",
-        }}
-        ref={searchButtonRef}
-        onClick={onOpen}>
-        <i sx={{ position: "absolute", left: "16px" }}>
-          <SearchIcon color="#A5A5A5" />
-        </i>
+          justifyContent: "space-between",
+        }}>
+        Search in docs
         <span
           sx={{
-            width: "100%",
-            height: "100%",
-            outline: "none",
-            backgroundColor: "docs.muted",
-            transition: "all 0.2s",
-            pl: "42px",
-            pr: "12px",
-            color: "docs.lightGray",
-            fontSize: "16px",
-            fontWeight: "300",
             display: "flex",
             alignItems: "center",
-            justifyContent: "space-between",
+            borderRadius: "12px",
+            border: "1px solid",
+            borderColor: "docs.border",
+            px: 2,
+            height: "26px",
+            transition: "all 0.2s",
           }}>
-          Search in docs
-          <span
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              borderRadius: "12px",
-              border: "1px solid",
-              borderColor: "docs.border",
-              px: 2,
-              height: "26px",
-              transition: "all 0.2s",
-            }}>
-            <span sx={{ variant: "srOnly" }}>Press </span>
-            <kbd>
-              <abbr
-                title="Command"
-                sx={{
-                  textDecoration: "none !important",
-                  mr: 1,
-                  fontSize: "22px",
-                }}>
-                ⌘
-              </abbr>
-            </kbd>
-            <span sx={{ variant: "srOnly" }}> and </span>
-            <kbd>K</kbd>
-            <span sx={{ variant: "srOnly" }}> to search</span>
-          </span>
+          <span sx={{ variant: "srOnly" }}>Press </span>
+          <kbd>
+            <abbr
+              title="Command"
+              sx={{
+                textDecoration: "none !important",
+                mr: 1,
+                fontSize: "22px",
+              }}>
+              ⌘
+            </abbr>
+          </kbd>
+          <span sx={{ variant: "srOnly" }}> and </span>
+          <kbd>K</kbd>
+          <span sx={{ variant: "srOnly" }}> to search</span>
         </span>
-      </button>
-      <Modal />
-    </>
+      </span>
+    </button>
   );
 };
 
