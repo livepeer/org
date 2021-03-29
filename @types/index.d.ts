3482455ca6aaa568fb2475f956ae3a721492a4f0
adamsoffer <adam@livepeer.org>
2021-03-29 12:25:35 -0400

    move types into @types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,3 +1,11 @@
+import { SxStyleProp } from "theme-ui";
+
+declare module "react" {
+  interface Attributes {
+    sx?: SxStyleProp;
+  }
+}
+
 declare module "*.gql" {
   const content: any;
   export default content;
--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { SxStyleProp } from "theme-ui";
-declare module "react" {
-  interface Attributes {
-    sx?: SxStyleProp;
-  }
-}
