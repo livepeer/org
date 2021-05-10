39b26fcd293a7c162bfdfd21761b7fafb6e81dc2
Adam Soffer <adam@livepeer.org>
2021-05-10 14:30:09 -0400

    fix vercel runtime error (#136)
    * fix vercel runtime error
    
    * include locale files in vercel's serverless output

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ['en', 'es', 'ru', 'zh', 'ko'],
     defaultLocale: 'en',
   },
+  localePath: path.resolve('./public/locales'),
   interpolation: {
     prefix: '{{',
     suffix: '}}',
