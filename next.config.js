7a758d5feb079082cf04030a3f85b83b90086e6b
Suhail Kakar <suhail.zahed@gmail.com>
2023-10-04 00:40:54 +0530

    Update next.config.js (#216)

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-const { i18n } = require('./next-i18next.config');
-const withPlugins = require('next-compose-plugins');
-const withSvgr = require('next-svgr');
-const withTM = require('next-transpile-modules')([
-  'gsap',
-  'gsap/DrawSVGPlugin',
-  'gsap/MotionPathPlugin',
-  'gsap/ScrollTrigger',
-  'gsap/SplitText',
-  'react-use-mailchimp',
-  'react-syntax-highlighter',
-  '@radix-ui/themes',
+const { i18n } = require("./next-i18next.config");
+const withPlugins = require("next-compose-plugins");
+const withSvgr = require("next-svgr");
+const withTM = require("next-transpile-modules")([
+  "gsap",
+  "gsap/DrawSVGPlugin",
+  "gsap/MotionPathPlugin",
+  "gsap/ScrollTrigger",
+  "gsap/SplitText",
+  "react-use-mailchimp",
+  "react-syntax-highlighter",
+  "@radix-ui/themes",
 ]);
 
 module.exports = withPlugins([
@@ -18,167 +18,175 @@ module.exports = withPlugins([
     {
       images: {
         domains: [
-          'assets.vercel.com',
-          'cdn.sanity.io',
-          'res.cloudinary.com',
-          'user-images.githubusercontent.com',
-          'i.imgur.com',
-          'placehold.co',
+          "assets.vercel.com",
+          "cdn.sanity.io",
+          "res.cloudinary.com",
+          "user-images.githubusercontent.com",
+          "i.imgur.com",
+          "placehold.co",
         ],
       },
-      pageExtensions: ['mdx', 'tsx'],
+      pageExtensions: ["mdx", "tsx"],
       webpack(config, _options) {
         config.module.rules.push({
           test: /\.mjs/,
           include: /node_modules/,
-          type: 'javascript/auto',
+          type: "javascript/auto",
         });
         config.module.rules.push({
           test: /\.(graphql|gql)$/,
           exclude: /node_modules/,
-          loader: 'graphql-tag/loader',
+          loader: "graphql-tag/loader",
         });
         config.module.rules.push({
           test: /\.md$/,
-          use: 'raw-loader',
+          use: "raw-loader",
         });
         return config;
+      },
+      async rewrites() {
+        return [
+          {
+            source: "/tv",
+            destination: "https://livepeer-tv.vercel.app",
+          },
+        ];
       },
       async redirects() {
         return [
           {
-            source: '/docs/:slug*',
-            destination: 'https://docs.livepeer.org/:slug*',
+            source: "/docs/:slug*",
+            destination: "https://docs.livepeer.org/:slug*",
             permanent: false,
           },
           {
-            source: '/tv',
-            destination: 'https://lvpr.tv/?v=7062iaygm8eip421',
+            source: "/tv",
+            destination: "https://lvpr.tv/?v=7062iaygm8eip421",
             permanent: false,
           },
           {
-            source: '/jobs/technical-writer',
-            destination: 'https://livepeer.org/jobs/1496366',
+            source: "/jobs/technical-writer",
+            destination: "https://livepeer.org/jobs/1496366",
             permanent: false,
           },
           {
-            source: '/jobs/full-stack-video-engineer',
-            destination: 'https://livepeer.org/jobs/1412799',
+            source: "/jobs/full-stack-video-engineer",
+            destination: "https://livepeer.org/jobs/1412799",
             permanent: false,
           },
           {
-            source: '/jobs/operations-manager',
-            destination: 'https://livepeer.org/jobs/1466566',
+            source: "/jobs/operations-manager",
+            destination: "https://livepeer.org/jobs/1466566",
             permanent: false,
           },
           {
-            source: '/jobs/video-developer-community-manager',
-            destination: 'https://livepeer.org/jobs/1476601',
+            source: "/jobs/video-developer-community-manager",
+            destination: "https://livepeer.org/jobs/1476601",
             permanent: false,
           },
           {
-            source: '/jobs/web3-developer-evangelist',
-            destination: 'https://livepeer.org/jobs/1491881',
+            source: "/jobs/web3-developer-evangelist",
+            destination: "https://livepeer.org/jobs/1491881",
             permanent: false,
           },
           {
-            source: '/jobs/chief-operating-officer',
-            destination: 'https://livepeer.org/jobs/1466562',
+            source: "/jobs/chief-operating-officer",
+            destination: "https://livepeer.org/jobs/1466562",
             permanent: false,
           },
           {
-            source: '/jobs/senior-video-infrastructure-engineer',
-            destination: 'https://livepeer.org/jobs/1414584',
+            source: "/jobs/senior-video-infrastructure-engineer",
+            destination: "https://livepeer.org/jobs/1414584",
             permanent: false,
           },
           {
-            source: '/jobs/video-developer-success-manager',
-            destination: 'https://livepeer.org/jobs/1476607',
+            source: "/jobs/video-developer-success-manager",
+            destination: "https://livepeer.org/jobs/1476607",
             permanent: false,
           },
           {
-            source: '/jobs/senior-software-engineer-video-transcoding',
-            destination: 'https://livepeer.org/jobs/1412803',
+            source: "/jobs/senior-software-engineer-video-transcoding",
+            destination: "https://livepeer.org/jobs/1412803",
             permanent: false,
           },
           {
-            source: '/jobs/analytics-engineer',
-            destination: 'https://livepeer.org/jobs/1496262',
+            source: "/jobs/analytics-engineer",
+            destination: "https://livepeer.org/jobs/1496262",
             permanent: false,
           },
           {
-            source: '/jobs/protocol-engineer',
-            destination: 'https://livepeer.org/jobs/1412804',
+            source: "/jobs/protocol-engineer",
+            destination: "https://livepeer.org/jobs/1412804",
             permanent: false,
           },
           {
-            source: '/jobs/investor-relations-manager',
-            destination: 'https://livepeer.org/jobs/1454503',
+            source: "/jobs/investor-relations-manager",
+            destination: "https://livepeer.org/jobs/1454503",
             permanent: false,
           },
           {
-            source: '/jobs/senior-product-marketing-manager',
-            destination: 'https://livepeer.org/jobs/1454194',
+            source: "/jobs/senior-product-marketing-manager",
+            destination: "https://livepeer.org/jobs/1454194",
             permanent: false,
           },
           {
-            source: '/jobs/senior-lead-product-manager',
-            destination: 'https://livepeer.org/jobs/1454194',
+            source: "/jobs/senior-lead-product-manager",
+            destination: "https://livepeer.org/jobs/1454194",
             permanent: false,
           },
           {
-            source: '/jobs/content-marketing',
-            destination: 'https://livepeer.org/jobs/1476609',
+            source: "/jobs/content-marketing",
+            destination: "https://livepeer.org/jobs/1476609",
             permanent: false,
           },
           {
-            source: '/jobs/marketing-manager',
-            destination: 'https://livepeer.org/jobs/1412808',
+            source: "/jobs/marketing-manager",
+            destination: "https://livepeer.org/jobs/1412808",
             permanent: false,
           },
           {
-            source: '/jobs/events-manager',
-            destination: 'https://livepeer.org/jobs/1454453',
+            source: "/jobs/events-manager",
+            destination: "https://livepeer.org/jobs/1454453",
             permanent: false,
           },
           {
-            source: '/jobs/engineering-manager-livepeer-core-software',
-            destination: 'https://livepeer.org/jobs/1478605',
+            source: "/jobs/engineering-manager-livepeer-core-software",
+            destination: "https://livepeer.org/jobs/1478605",
             permanent: false,
           },
           {
-            source: '/jobs/technical-product-manager-orchestrator-experience',
-            destination: 'https://livepeer.org/jobs/1496214',
+            source: "/jobs/technical-product-manager-orchestrator-experience",
+            destination: "https://livepeer.org/jobs/1496214",
             permanent: false,
           },
           {
-            source: '/changelog',
-            destination: 'https://livepeer.canny.io/changelog',
+            source: "/changelog",
+            destination: "https://livepeer.canny.io/changelog",
             permanent: false,
           },
           {
-            source: '/feature-requests',
-            destination: 'https://livepeer.canny.io/feature-requests',
+            source: "/feature-requests",
+            destination: "https://livepeer.canny.io/feature-requests",
             permanent: false,
           },
           {
-            source: '/faq',
-            destination: 'https://docs.livepeer.org',
+            source: "/faq",
+            destination: "https://docs.livepeer.org",
             permanent: false,
           },
           {
-            source: '/video-miners',
-            destination: 'https://livepeer.org/orchestrators',
+            source: "/video-miners",
+            destination: "https://livepeer.org/orchestrators",
             permanent: false,
           },
           {
-            source: '/tokenholders',
-            destination: 'https://livepeer.org/delegators',
+            source: "/tokenholders",
+            destination: "https://livepeer.org/delegators",
             permanent: false,
           },
           {
-            source: '/ecosystem',
-            destination: 'https://livepeer.org/ecosystem/showcase',
+            source: "/ecosystem",
+            destination: "https://livepeer.org/ecosystem/showcase",
             permanent: false,
           },
         ];
