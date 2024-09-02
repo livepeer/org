f9f354635109bff44efd1b67e9cd9add8f68e82f
Suhail Kakar <suhail.zahed@gmail.com>
2024-09-02 18:33:53 +0530

    ui: add size limit error

--- components/sections/jobs/form.tsx
+++ components/sections/jobs/form.tsx
@@ -150,6 +150,13 @@ const JobApplicationForm = ({
       setLoadingPdf(true);
       setError(null);
       let form = new FormData();
+      if (acceptedFiles[0].size > 4 * 1024 * 1024) {
+        setError(
+          "Your CV file is too large. Please upload a file smaller than 4MB."
+        );
+        setLoadingPdf(false);
+        return;
+      }
       form.append("file", acceptedFiles[0]);
       form.append("file_name", acceptedFiles[0].name);
       await fetch("/api/upload", {
@@ -432,7 +439,13 @@ const JobApplicationForm = ({
                 </Box>
               ))}
 
-            <Box>{error}</Box>
+            <Box
+              sx={{
+                color: "red",
+                marginTop: 3,
+              }}>
+              {error}
+            </Box>
             <Button
               sx={{ marginTop: 4, paddingX: 3, width: "100%" }}
               onClick={onClick}
